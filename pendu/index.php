d8b80da952842a95fad607e08705abe143353d6d
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-11-13 23:24:05 +0100

    Delete index.php

--- pendu/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-set_include_path($_SERVER['DOCUMENT_ROOT']."/jeux");
-echo(str_replace("dos", "active", str_replace("%php%", file_get_contents("page.html"), file_get_contents("header.html", true))));
-?>
